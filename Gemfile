b662c915ece026652d24c550a3d04e48ab7368d7
Takeshi Yabe <tyabe@nilidea.com>
2012-04-03 02:27:37 +0900

    initial commit
    padrino g project kaja -t rspec -e haml -c scss -s jquery -d mini_record

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source :rubygems
+
+# Server requirements
+# gem 'thin' # or mongrel
+# gem 'trinidad', :platform => 'jruby'
+
+# Project requirements
+gem 'rake'
+gem 'sinatra-flash', :require => 'sinatra/flash'
+
+# Component requirements
+gem 'sass'
+gem 'haml'
+gem 'mini_record'
+gem 'sqlite3'
+
+# Test requirements
+gem 'rspec', :group => "test"
+gem 'rack-test', :require => "rack/test", :group => "test"
+
+# Padrino Stable Gem
+gem 'padrino', '0.10.6'
+
+# Or Padrino Edge
+# gem 'padrino', :git => 'git://github.com/padrino/padrino-framework.git'
+
+# Or Individual Gems
+# %w(core gen helpers cache mailer admin).each do |g|
+#   gem 'padrino-' + g, '0.10.6'
+# end
---
+++ config/apps.rb
@@ -0,0 +1,34 @@
+##
+# This file mounts each app in the Padrino project to a specified sub-uri.
+# You can mount additional applications using any of these commands below:
+#
+#   Padrino.mount("blog").to('/blog')
+#   Padrino.mount("blog", :app_class => "BlogApp").to('/blog')
+#   Padrino.mount("blog", :app_file =>  "path/to/blog/app.rb").to('/blog')
+#
+# You can also map apps to a specified host:
+#
+#   Padrino.mount("Admin").host("admin.example.org")
+#   Padrino.mount("WebSite").host(/.*\.?example.org/)
+#   Padrino.mount("Foo").to("/foo").host("bar.example.org")
+#
+# Note 1: Mounted apps (by default) should be placed into the project root at '/app_name'.
+# Note 2: If you use the host matching remember to respect the order of the rules.
+#
+# By default, this file mounts the primary app which was generated with this project.
+# However, the mounted app can be modified as needed:
+#
+#   Padrino.mount("AppName", :app_file => "path/to/file", :app_class => "BlogApp").to('/')
+#
+
+##
+# Setup global project settings for your apps. These settings are inherited by every subapp. You can
+# override these settings in the subapps as needed.
+#
+Padrino.configure_apps do
+  # enable :sessions
+  set :session_secret, '4a06000bc28a572317f9bea2953ca6be932fec7a71f2b960e61c2455a89c2dd1'
+end
+
+# Mounts the core application for this project
+Padrino.mount("Kaja").to('/')
