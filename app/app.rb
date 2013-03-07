1f2f1a41b9656dd1c3b9ee0e9e18b381e86d15eb
Takeshi Yabe <tyabe@nilidea.com>
2013-03-08 02:14:28 +0900

    Move routes to base controller from app.rb

--- app/app.rb
+++ app/app.rb
@@ -19,38 +19,9 @@ class Kaja < Padrino::Application
 
   enable :caching
 
-  controller cache: true do
-
-    before do
-      I18n.locale = (params[:captures] && s=params[:captures].first) ? s.gsub("/","").to_sym : :ja
-      expires_in 60
-    end
-
-    get %r{/(en)?} do
-      @page_id = 'Index'
-      render :index
-    end
-
-    get %r{/(en/)?about} do
-      @page_id = 'About'
-      render :about
-    end
-
-    get %r{/(en/)?entry} do
-      @page_id = 'Entry'
-      render :entry
-    end
-
-    get %r{/(en/)?faq} do
-      @page_id = 'Faq'
-      render :faq
-    end
-
-    get %r{/(en/)?kaja} do
-      @page_id = 'Kaja'
-      @nominees = Nominee.of(2012)
-      render :kaja
-    end
+  before do
+    I18n.locale = (params[:captures] && s=params[:captures].first) ? s.gsub("/","").to_sym : :ja
+    expires_in 60
   end
 
   private
